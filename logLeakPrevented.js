3ff068add9cf03135e33172f1d71cf54f77afdca
Aaditya-Hingmire <Aadityahingmire@gmail.com>
2026-03-14 01:26:19 +0530

    Delete logLeakPrevented.js

--- logLeakPrevented.js
+++
@@ -1,9 +0,0 @@
-function logLeakPrevented(type) {
-    chrome.storage.local.get(['stats'], (result) => {
-        let stats = result.stats || { total: 0, types: {} };
-        stats.total++;
-        stats.types[type] = (stats.types[type] || 0) + 1;
-        
-        chrome.storage.local.set({ stats: stats });
-    });
-}
